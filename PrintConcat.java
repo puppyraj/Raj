3c9ec265ef85c6d5594819d7fe29fb88cab8321f
puppyraj <rajsnackii@gmail.com>
2022-09-22 12:00:49 +0530

    string1

--- PrintConcat.java
+++ PrintConcat.java
@@ -2,8 +2,8 @@ package Day3;
 
 public class PrintConcat {
 	public static void main(String[]args) {
-		String name1 = "Bhuvaneshwari";
-		String name2 = "RajKumar";
+		String name1 = "Bhuvi";
+		String name2 = "Raj";
 		String result = name1.concat(name2);
 		System.out.println(result);
 		
